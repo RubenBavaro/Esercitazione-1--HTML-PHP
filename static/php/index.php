b46bbc910f85aa0055ed63aac8ea9df98beb54ae
RubenBavaro <bavaro.ruben@panettipitagora.edu.it>
2025-09-23 15:36:28 +0200

    Finished

--- static/php/index.php
+++ static/php/index.php
@@ -11,52 +11,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $climate = isset($_POST['climate']) ? $_POST['climate'] : '';
     $altitude = isset($_POST['altitude']) ? trim($_POST['altitude']) : '';
 
-    $errors = [];
 
-    if (empty($location)) {
-        $errors[] = "La località è obbligatoria.";
-    }
-
-    if (empty($date)) {
-        $errors[] = "La data è obbligatoria.";
-    } else {
         $dateObj = DateTime::createFromFormat('Y-m-d', $date);
-        if (!$dateObj) {
-            $errors[] = "Formato data non valido.";
-        } else {
-            $today = new DateTime();
+        $today = new DateTime();
             $today->setTime(0, 0, 0);
-            if ($dateObj > $today) {
-                $errors[] = "La data non può essere futura.";
-            }
-        }
-    }
-
-    if (empty($time)) {
-        $errors[] = "L'ora è obbligatoria.";
-    }
-
-    if (empty($type)) {
-        $errors[] = "Il tipo di rivelazione è obbligatorio.";
-    }
-
-    if (empty($humidity)) {
-        $errors[] = "L'umidità è obbligatoria.";
-    }
-
-    if (empty($climate)) {
-        $errors[] = "Il clima attuale è obbligatorio.";
-    }
-
-    if (empty($altitude)) {
-        $errors[] = "L'altitudine è obbligatoria.";
-    } elseif (!is_numeric($altitude) || $altitude < 0) {
-        $errors[] = "L'altitudine deve essere un numero positivo.";
-    }
-
-    // If no errors, process the data
-    if (empty($errors)) {
-        // Format the date for display
         $formattedDate = $dateObj->format('d/m/Y');
 
         // Create type description
@@ -79,17 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $output .= "<p><strong>Clima Attuale:</strong> $climate</p>";
         $output .= "<p><strong>Altitudine:</strong> $altitude m</p>";
         $output .= "</div>";
-    } else {
-        // Display errors
-        $output = "<div class='alert alert-danger'>";
-        $output .= "<h3>Errore nella validazione:</h3>";
-        $output .= "<ul>";
-        foreach ($errors as $error) {
-            $output .= "<li>$error</li>";
-        }
-        $output .= "</ul>";
-        $output .= "</div>";
-    }
 }
 ?>
 
@@ -110,10 +57,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <?php if (!empty($output)) {
                 echo $output;
             } ?>
-
-            <div class="mt-4">
-                <a href="../index.html" class="btn btn-primary">Nuova Rivelazione</a>
-            </div>
         </div>
     </div>
 </div>
